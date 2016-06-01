2ab40a20c45acf312f13b2f4c449a9840b8ee6b6
shahrukh <msr.concordfly@gmail.com>
2016-06-02 00:17:45 +0530

    Amazon Scrapped

--- scrap/views.py
+++ scrap/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+import json
+from django.http import JsonResponse
 # Create your views here.
 def home(request):
 	context = {}
@@ -10,6 +12,32 @@ def home(request):
 
 def scraper(request):
 	if request.method == 'POST':
-		url = "http://kevin.schaul.io/2011/11/07/tutorial-web-scraping-with-django/"
-		html_code = requests.get(url)
-		print html_code.content
+		amazon_url = "http://www.amazon.in/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords="
+
+		try:
+			query = json.loads(request.body).get('query')
+		except:
+			query = ""
+
+		if query:
+			amazon_url = amazon_url + query
+			amazon_html = requests.get(amazon_url)
+			amazon_data = BeautifulSoup(amazon_html.content  ,"lxml")
+			x = amazon_data.find_all("li", {"class":"s-result-item celwidget"})
+			models = []
+			price = []
+			for i in x:
+				models.append(i.contents[0].find_all("a", {"class":"a-link-normal s-access-detail-page  a-text-normal"})[0].text)
+				# print i.contents[0].find_all("a", {"class":"a-link-normal s-access-detail-page  a-text-normal"})[0].text		
+				# print i.contents[0].find_all("span", {"class":"a-size-base a-color-price s-price a-text-bold"})[0].text
+				try:
+					price.append(i.contents[0].find_all("span", {"class":"a-size-base a-color-price s-price a-text-bold"})[0].text)
+				except:
+					price.append("Price not mentioned")
+
+			amazon = {"models" : models , "price" : price}
+
+			return JsonResponse({'amazon':amazon})
+
+
+
--- static/js/home.js
+++ static/js/home.js
@@ -1,24 +1,32 @@
 $(function(){
-var x = '{% url "scraper" %}';
+
 console.log(document.getElementsByName('csrfmiddlewaretoken')[0].value);
-console.log(x);
-var arr = {
-		csrfmiddlewaretoken:document.getElementsByName('csrfmiddlewaretoken')[0].value,
-		query : $('#query').val(),
-}
 
 $('#submit').click(function(){
 
+var arr = {
+		csrfmiddlewaretoken:document.getElementsByName('csrfmiddlewaretoken')[0].value,
+		query : $('#query').val(),
+};
+var temp = {
+	'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value
+};
 
 $.ajax({
-	url : '{% url "home" %}',
+	url : '/app/scrap/',
 	type: 'POST',
 	dataType: 'json',
 	async: false,
+	headers: temp,
 	data: JSON.stringify(arr),
     contentType: 'application/json; charset=utf-8', 
 	success: function(data){
+		// console.log(data['amazon']);
 
+		for (i = 0; i<data['amazon']['models'].length - 1; i++)
+			{	
+				$('#items').append('<li><h3>' + data["amazon"]["models"][i] + '</h3><h5>'+data["amazon"]["price"][i]+'</h5></li>')
+			}
 	}
 
 });
